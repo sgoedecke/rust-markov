bb3c8471cba5d6e1f603dbef360ef01fad9262c3
Sean Goedecke <sgoedecke@zendesk.com>
2018-12-30 15:22:42 +1100

    fix panics and extract sentence building

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,30 @@ fn main() {
     let contents = fs::read_to_string(filename)
         .expect("Could not read file");
 
-    let first_words: Vec<&str> = contents.split('.')
+    let first_words: Vec<&str> = contents.trim().split('.')
         .collect::<Vec<&str>>()
         .iter()
-        .map(|s| s.trim().split(' ').next().unwrap()).collect();
+        .map(|s| s.trim().split(' ').next().unwrap())
+        .filter(|s| s != &"")
+        .collect();
 
     let mut lookup_table: HashMap<&str, Vec<&str>> = HashMap::new();
-    let words: Vec<&str> = contents.split(' ').collect();
+    let words: Vec<&str> = contents.trim().split(' ').collect();
     for (i, word) in words.iter().enumerate() {
         if i+1 < words.len() { // TODO: guard clause
             lookup_table.entry(word).or_insert([].to_vec()).push(words[i+1]);
         }
     }
 
+    let mut output = String::from("");
+    for _x in 0..10 {
+        output.push_str(&generate_sentence(&first_words, &lookup_table));
+        output.push(' ');
+    }
+    println!("{}", output);
+}
 
+fn generate_sentence(first_words: &Vec<&str>, lookup_table: &HashMap<&str, Vec<&str>>) -> String {
     let mut sentence: Vec<&str> = [].to_vec();
     sentence.push(rand::thread_rng().choose(&first_words).unwrap());
     loop {
@@ -35,6 +45,5 @@ fn main() {
         let candidates = lookup_table.get(last_word).unwrap();
         sentence.push(rand::thread_rng().choose(&candidates).unwrap());
     }
-
-    println!("{:?}", sentence.join(&String::from(" ")));
+    sentence.join(&String::from(" "))
 }
